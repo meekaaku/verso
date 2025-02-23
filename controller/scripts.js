6e044392252ed5625910c478fb95bb6848228abb
meekaaku <meekaaku@gmail.com>
2025-02-23 23:50:11 +0000

    Added plain html/js controller

---
+++ controller/scripts.js
@@ -0,0 +1,44 @@
+function startConnection() {
+    ws = new WebSocket('ws://rook:8765');
+    const statusElement = document.getElementById('connection-status');
+
+    // WebSocket event handlers
+    ws.onopen = () => {
+        statusElement.textContent = 'Connected';
+        statusElement.classList.remove('disconnected');
+        statusElement.classList.add('connected');
+    };
+
+    ws.onclose = () => {
+        statusElement.textContent = 'Disconnected';
+        statusElement.classList.remove('connected');
+        statusElement.classList.add('disconnected');
+    };
+
+    ws.onclose = () => {
+        statusElement.textContent = 'Disconnected - Attempting to reconnect...';
+        statusElement.classList.remove('connected');
+        statusElement.classList.add('disconnected');
+        
+        setTimeout(() => {
+            window.location.reload();
+        }, 5000);
+    };
+    // Function to send slider data
+    
+
+}
+
+function sendSliderData(n) {
+    console.log("Sending slider data", n);
+    const valueElement = document.getElementById('value' + n);
+    const sliderElement = document.getElementById('slider' + n);
+    valueElement.textContent = sliderElement.value;
+    if (ws.readyState === WebSocket.OPEN) {
+        const data = {
+            joint: n,
+            value: Number(sliderElement.value)
+        };
+        ws.send(JSON.stringify(data));
+    }
+}
